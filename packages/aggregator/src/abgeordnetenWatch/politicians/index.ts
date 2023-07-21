1a4beff45cbb51b77da8a2377113b490b9e6ad9c
Duc Tai Ly <ductaily@yahoo.de>
2023-07-21 23:27:23 +0200

    add politician kpis

--- packages/aggregator/src/abgeordnetenWatch/politicians/index.ts
+++ packages/aggregator/src/abgeordnetenWatch/politicians/index.ts
@@ -1,13 +1,83 @@
 import { customFetch, writeFileAndArchive } from '../../utils';
 
+import { PoliticianResults } from '../types/politician';
 import { join } from 'path';
 
 const basePath = join(__dirname, '..', '..', '..', 'data', 'politicians');
 
+function sanitizeOccupation(occupation: string): string {
+  if (!occupation) {
+    return 'unknown';
+  }
+  if (occupation.startsWith('Student')) {
+    return 'Student';
+  }
+  if (occupation.startsWith('Steuerfachangestellt')) {
+    return 'Steuerfachangestellt';
+  }
+
+  return occupation;
+}
+
+function sanitizeEducation(education: string | null): string {
+  if (!education) {
+    return 'unknown';
+  }
+  if (education.startsWith('Rechtsanw')) {
+    return 'Rechtsanwalt';
+  }
+
+  return education;
+}
+
+async function aggregateByEducation(
+  today: string,
+  politicians: PoliticianResults,
+): Promise<void> {
+  const aggregate: { [key: string]: number[] } = {};
+
+  for (const politician of politicians.data) {
+    const key = sanitizeEducation(politician.education);
+    if (!aggregate[key]) {
+      aggregate[key] = [];
+    }
+    aggregate[key].push(politician.id);
+  }
+
+  await writeFileAndArchive(
+    today,
+    JSON.stringify(aggregate, null, 2),
+    'politiciansByEducation.json',
+    basePath,
+  );
+}
+
+async function aggregateByOccupation(
+  today: string,
+  politicians: PoliticianResults,
+): Promise<void> {
+  const aggregate: { [key: string]: number[] } = {};
+
+  for (const politician of politicians.data) {
+    const key = sanitizeOccupation(politician.occupation);
+    if (!aggregate[key]) {
+      aggregate[key] = [];
+    }
+    aggregate[key].push(politician.id);
+  }
+
+  await writeFileAndArchive(
+    today,
+    JSON.stringify(aggregate, null, 2),
+    'politiciansByOccupation.json',
+    basePath,
+  );
+}
+
 export async function fetchPoliticians(): Promise<void> {
   const today = new Date().toISOString().slice(0, 10);
 
-  const politicians = await customFetch(
+  const politicians: PoliticianResults = await customFetch(
     'https://www.abgeordnetenwatch.de/api/v2/politicians',
   );
 
@@ -18,5 +88,7 @@ export async function fetchPoliticians(): Promise<void> {
       'source.json',
       basePath,
     ),
+    aggregateByOccupation(today, politicians),
+    aggregateByEducation(today, politicians),
   ]);
 }
--- packages/aggregator/src/abgeordnetenWatch/types/politician.ts
+++ packages/aggregator/src/abgeordnetenWatch/types/politician.ts
@@ -1,4 +1,5 @@
 import { BaseObject } from './base';
+import { Metadata } from './metadata';
 import { PartyLink } from './party';
 
 export interface Politician extends BaseObject {
@@ -6,6 +7,17 @@ export interface Politician extends BaseObject {
   last_name: string;
   birth_name: string | null;
   sex: string;
-  year_of_birth: string;
+  year_of_birth: number | null;
   party: PartyLink;
+  education: string | null;
+  residence: string;
+  occupation: string;
+  statistic_questions: number | null;
+  statistic_questions_answered: number | null;
+  ext_id_bundestagsverwaltung: string | null;
+}
+
+export interface PoliticianResults {
+  meta: Metadata;
+  data: Politician[];
 }
