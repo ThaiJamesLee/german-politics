0c718aa67d6352c1bde949b28ec62a7ae6b60573
Duc Tai Ly <ductaily@yahoo.de>
2023-07-21 22:16:52 +0200

    Updates

--- packages/aggregator/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import fetch, { RequestInfo, RequestInit } from "node-fetch";
-
-export async function customFetch<T>(url: RequestInfo, init?: RequestInit): Promise<T> {
-    const response = await fetch(url, init)
-    if(response.ok) {
-        return response.json()
-    }
-    throw new Error(response.statusText)
-}
