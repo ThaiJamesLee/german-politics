c712b1d48b2fb9fb1c91fa4288204d94fdb805a6
Duc Tai Ly <ductaily@yahoo.de>
2023-08-07 22:20:43 +0200

    add average age

--- packages/ui/src/home/Home.tsx
+++ packages/ui/src/home/Home.tsx
@@ -1,14 +1,51 @@
-import { FlexBox, List, StandardListItem } from "@ui5/webcomponents-react";
+import {
+  Card,
+  CardHeader,
+  FlexBox,
+  FlexBoxWrap,
+  List,
+  StandardListItem,
+} from "@ui5/webcomponents-react";
+import {
+  fetchAllPoliticians,
+  fetchParliaments,
+} from "../components/fetchParliamentData";
 
 import CustomPage from "../components/Page";
-import { fetchParliaments } from "../components/fetchParliamentData";
+import { RETIREMENT_AGE } from "./constants";
+import { RadialChart } from "@ui5/webcomponents-react-charts";
 import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 
+const AveragePoliticianAgeChart = () => {
+  const allPoliticians = useQuery("allPoliticians", fetchAllPoliticians);
+
+  const averageAge = useMemo(() => {
+    if (!allPoliticians.data) return 0;
+    const numberPoliticiansWithBirthdate = allPoliticians.data.filter((entry) =>
+      Boolean(entry.data.year_of_birth)
+    );
+    const currentYear = new Date().getFullYear();
+
+    return Math.round(
+      numberPoliticiansWithBirthdate.reduce((acc, curr) => {
+        return acc + (currentYear - curr.data.year_of_birth);
+      }, 0) / numberPoliticiansWithBirthdate.length
+    );
+  }, [allPoliticians]);
+
+  return (
+    <RadialChart
+      color="#f0ab00"
+      value={Math.round((100 * averageAge) / RETIREMENT_AGE)}
+      displayValue={`${averageAge} Years`}
+    />
+  );
+};
+
 const Home = () => {
   const parliaments = useQuery("parliaments", fetchParliaments);
-
   const navigate = useNavigate();
 
   const onClickParliamentHandler = (parliamentId: string) => {
@@ -23,19 +60,31 @@ const Home = () => {
 
   return (
     <CustomPage>
+      <FlexBox wrap={FlexBoxWrap.Wrap}>
+        <Card
+          header={
+            <CardHeader titleText="Average age of politicians in all parliaments"></CardHeader>
+          }
+          style={{ width: "400px" }}
+        >
+          <AveragePoliticianAgeChart />
+        </Card>
+      </FlexBox>
       <FlexBox wrap="Wrap">
-        <List headerText="Parliaments">
-          {parliamentList.map((entry) => {
-            return (
-              <StandardListItem
-                key={entry}
-                onClick={onClickParliamentHandler.bind(null, entry)}
-              >
-                {entry}
-              </StandardListItem>
-            );
-          })}
-        </List>
+        <Card>
+          <List title="Parliaments">
+            {parliamentList.map((entry) => {
+              return (
+                <StandardListItem
+                  key={entry}
+                  onClick={onClickParliamentHandler.bind(null, entry)}
+                >
+                  {entry}
+                </StandardListItem>
+              );
+            })}
+          </List>
+        </Card>
       </FlexBox>
     </CustomPage>
   );
---
+++ packages/ui/src/home/constants.ts
@@ -0,0 +1,1 @@
+export const RETIREMENT_AGE = 66;
